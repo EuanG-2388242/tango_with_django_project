14bf54353c81c671f0818675df85d9244e535f54
Euan Galloway <2388242@student.gla.ac.uk>
2025-02-05 21:47:57 +0000

    Chapter 9 - Bug Fix Commint - Issue with Base.html link not actually showing up on webpages fixed

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
 from rango.models import Category, Page
-from rango.forms import CategoryForm, PageForm
-from django.shortcuts import redirect
+from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 def index(request):
     #Construct a dictionary to pass to the template engine as its context.
@@ -94,3 +93,39 @@ def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Euan'}
 
     return render(request, 'rango/about.html', context=context_dict)
+
+def register(request):
+
+    registered = False
+
+    if request.method == 'POST':
+
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+
+            registered = True
+        
+        else:
+
+            print(user_form.errors, profile_form.errors)
+        
+    else:
+
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'rango/register.html', context = {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
